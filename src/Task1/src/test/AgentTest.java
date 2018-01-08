b56e5f7dbc4158204a1c9c6dcc3210904c6204eb
Roderick Micallef <roderick.micallef.15@um.edu.mt>
2018-01-08 23:16:17 +0100

    Modified Tests

--- src/Task1/src/test/AgentTest.java
+++ src/Task1/src/test/AgentTest.java
@@ -4,25 +4,28 @@ import main.Agent;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 
 public class AgentTest{
     private Agent a;
 
-    @Before
-    public void Before(){
-        a = new Agent();
-    }
-
     @Test
     public void LoginSuccess(){
-        a.id = "123";
+        a = new Agent("123","name");
+        InputStream in = new ByteArrayInputStream(a.getID().getBytes());
+        a.setScanner(new Scanner(in));
         assertEquals(true,a.login());
     }
 
     @Test
     public void LoginFailure(){
-        a.id = "spy-123";
+        a = new Agent("spy-123","name");
+        InputStream in = new ByteArrayInputStream(a.getID().getBytes());
+        a.setScanner(new Scanner(in));
         assertEquals(false,a.login());
     }
 
--- src/Task1/src/test/SupervisorTest.java
+++ src/Task1/src/test/SupervisorTest.java
@@ -2,31 +2,22 @@ package test;
 
 import main.Agent;
 import main.Supervisor;
-import main.SupervisorStub;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SupervisorTest{
-    private Supervisor s;
     private Agent a;
-
-    @Before
-    public void Before(){
-        s = new SupervisorStub();
-    }
 
     @Test
     public void LoginSuccess(){
         a = new Agent("123","name");
-        assertEquals(true,s.getLoginKey(a));
+        assertEquals(true,Supervisor.getLoginKey(a));
     }
 
     @Test
     public void LoginFailure(){
         a = new Agent("123","name");
-        assertEquals(false,s.getLoginKey(a));
+        assertEquals(false,Supervisor.getLoginKey(a));
     }
 }
