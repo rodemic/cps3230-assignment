e7e9bbdeba7e5c8ce3db6c215934c5872d381c3e
DESKTOP-U7T3T39\thoma <thomas.m.mallia@gmail.com>
2018-01-08 20:10:24 +0100

    added logintoken, message, supervisorstub and removed cps3230-assignment.iml

---
+++ src/Task1/src/main/LoginTokin.java
@@ -0,0 +1,7 @@
+package main;
+
+public class LoginTokin {
+    String loginkey;
+    String agentId;
+    long timestamp;
+}
---
+++ src/Task1/src/main/Message.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Message {
+    String sourceAgentId;
+    String targetAgentId;
+    long timestamp;
+}
